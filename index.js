7224a02ecae273045af9ac0829e28931a1a1ac81
Landon Siler <silerlandon@gmail.com>
2020-08-15 01:44:14 -0400

    Added New Features To The Website

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const button = document.querySelector(".myB");
-button.onclick = function () {
-
+const link = document.querySelector(".myBDiv");
+const link2 = document.querySelector(".myBDivi");
+    link.onmouseover = function () {
+        link.style.top = '4.8em';
 };
-
+link2.onmouseover = function () {
+    link2.style.top = '7.8em';
+};
+link.onmouseout = function () {
+    link.style.top = '5em';
+};
+link2.onmouseout = function () {
+    link2.style.top = '8em';
+};
 
 
