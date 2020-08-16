80b02c87ca682657543ce3823c274e1082aea255
Landon Siler <silerlandon@gmail.com>
2020-08-16 02:10:25 -0400

    Added Menu Buttons And Interactivity

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const link = document.querySelector(".myBDiv");
-const link2 = document.querySelector(".myBDivi");
-    link.onmouseover = function () {
-        link.style.top = '4.8em';
-};
-link2.onmouseover = function () {
-    link2.style.top = '7.8em';
-};
-link.onmouseout = function () {
-    link.style.top = '5em';
-};
-link2.onmouseout = function () {
-    link2.style.top = '8em';
-};
+
 
 
